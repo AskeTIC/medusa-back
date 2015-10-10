f9c315b80fc937f2aae12abd37efa459d8875264
jonabasque <jonmultimedia@gmail.com>
2015-10-10 08:18:58 +0200

    Añadido logs y comentarios y evento data-sensor1

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-//Librerías
+//LIBRERIAS
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-//Eventos y rutas
-io.on('connection', function(){  
+//EVENTOS
+//Cuando se establece una conexión desde un cliente...
+io.on('connection', function(socket){  
+	//Muestro un aviso en backend 
 	console.log("Cliente conectado!");
+	//Emito al cliente un evento y un mensaje
+	socket.emit('conexion-realizada', 'Conectado con éxito');
+
+	//Cuando recibo datos...
+	socket.on('data-sensor1', function(){
+		console.log('guardar en BBDD y enviar a cliente.');
+	});
+
+	//Cuando se desconecta un cliente...
+	socket.on('disconnect', function () {
+    		io.emit('user disconnected');
+  	});
 });
 
-//Puesta en escucha del server
-server.listen(3030);
+//PUESTA EN ESCUCHA DEL SERVER
+server.listen(3030, function() {  
+    console.log('Servidor corriendo http://localhost:3030');
+});
