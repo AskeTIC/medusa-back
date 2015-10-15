a8fa74f93f6a28dbaf6a10bce869f265e2cbf75a
jonabasque <jonmultimedia@gmail.com>
2015-10-15 14:26:49 +0200

    Mejora en definición de eventos

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var routes = require('./routes/routes');
 
 //Definición de namesapaces para sokets que vamos a usar.
 var sensors = io.of('/sensors');
-var users = io.of('/');
+var users = io.of('/orders');
 
 //MIDDLEWARES
 app.use(express.static('../medusa-front'));
@@ -19,12 +19,51 @@ app.use(express.static('../medusa-front'));
 app.use('/', routes.router);
 
 //EVENTOS
+
+//NAMESPACE '/orders' QUE ES PARA LOS CONTROLADORES
 //Cuando se establece una conexión desde un cliente user...
 users.on('connection', function(socket){
 	//Muestro un aviso en backend
-	console.log('Usuario conectado!');
+	console.log('cliente conectado!');
 	//Emito al usuario conectado un evento y un mensaje
 	socket.emit('conexion realizada', 'Conectado con éxito');
+
+	//ORDENES ESPECIFICAS DESDE EL USUARIO
+	//Cuando recibo el evento carga-controller...
+	socket.on('carga-controller', function(orden){
+
+		console.log('carga-controller :' + orden)
+	});
+
+	//Cuando recibo el evento desplazamiento-controller...
+	socket.on('dezplazamiento-controller', function(orden){
+
+		console.log('desplazamiento-controller :' + orden)
+	});
+
+	//Cuando recibo el evento vela-controller...
+	socket.on('vela-controller', function(orden){
+
+		console.log('vela-controller :' + orden)
+	});
+
+	//Cuando recibo el evento pertiga-controller...
+	socket.on('pertiga-controller', function(orden){
+
+		console.log('pertiga-controller :' + orden)
+	});
+
+
+	//ORDENES GENERALES: SISTEMAS, ETC..
+	//Cuando recibo el evento sistems-controller...
+	socket.on('sistems-controller', function(data){
+		var status = false;
+		if(status !== true){
+			socket.emit('sistems-up', data);//TODO: Pasarle algo? En principio creo que dependiendo la fase o ensayo quizás haya que crear algo al respecto.
+		}else{
+			socket.emit('sistems-down', data)//TODO: Pasarles algo?
+		}
+	});
 
 	//Cuando se desconecta el usuario...
 	socket.on('disconnect', function () {
@@ -33,6 +72,7 @@ users.on('connection', function(socket){
   	});
 });
 
+// NAMESPACE '/sensors' QUE ES PARA LOS SENSORES
 // cuando se establece una conexión desde un cliente sensor...
 sensors.on('connection', function(socket){
 	//Muestro un aviso en el backend
@@ -40,20 +80,44 @@ sensors.on('connection', function(socket){
 	//Emito al sensor un evento y un mensaje para actuar en consecuencia.
 	socket.emit('conexion-realizada', 'Conectado');
 
-	//Cuando recibo datos del sensor1...
-    socket.on('data-sensor1', function(){
+	//RECIBO LOS DATOS DE LOS SENSORES
+	//Cuando recibo datos del termomentro..
+    socket.on('data-temperature', function(data){
         //TODO: guardar el BBDD y enviar al cliente correspondiente.
-        console.log('guardar en BBDD y enviar a cliente.');
+        console.log('data-temperature : '+data);
     });
+
+	//Cuando recibo datos del inclinometro norte...
+	socket.on('data-inclinometro-norte', function(data){
+		//TODO: guardar el BBDD y enviar al cliente correspondiente.
+		console.log('data-inclinometro-norte : '+data);
+	});
+
+	//Cuando recibo datos del incinometro sur...
+	socket.on('data-inclinometro-sur', function(data){
+		//TODO: guardar el BBDD y enviar al cliente correspondiente.
+		console.log('data-inclinometro-sur : '+data);
+	});
+
+	//Cuando recibo datos del sensor1...
+	socket.on('data-incinometro-este', function(data){
+		//TODO: guardar el BBDD y enviar al cliente correspondiente.
+		console.log('data-inclinometro-este : '+data);
+	});
+
+	//Cuando recibo datos del inclinometro oeste...
+	socket.on('data-inclinometro-oeste', function(data){
+		//TODO: guardar el BBDD y enviar al cliente correspondiente.
+		console.log('data-inclinometro-oeste : '+data);
+	});
 
 	//Cuando se desconecta el sensor...
 	socket.on('disconnect', function () {
         //Haré algo al respecto, si pogo io.emit envío a todos los usuarios
-        socket.emit('user disconnected'); //En este caso me interesará avis$
+        socket.emit('sensor disconnected'); //En este caso me interesará enviar a los clientes usuarios no sensores.
     });
 
 });
-
 
 
 
