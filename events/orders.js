09d51b922d33235977c8fa9ee8ee645431220a48
jonabasque <jonmultimedia@gmail.com>
2015-10-21 14:32:23 +0200

    Añadidas inserciones de documentos en eventos orders.js

--- events/orders.js
+++ events/orders.js
@@ -16,29 +16,26 @@ module.exports = function Orders(){
             //ORDENES ESPECIFICAS DESDE EL USUARIO
             //Cuando recibo el evento carga-controller...
             socket.on('carga-controller', function(orden){
-
+                orders.postDocuments(orden, orders.cbPostDocuments);
                 console.log('carga-controller :' + orden);
-                orders.postDocuments(orden, orders.cbPostDocuments)
             });
 
             //Cuando recibo el evento desplazamiento-controller...
             socket.on('desplazamiento-controller', function(orden){
-
+                orders.postDocuments(orden, orders.cbPostDocuments);
                 console.log('desplazamiento-controller :' + orden);
             });
 
             //Cuando recibo el evento vela-controller...
             socket.on('vela-controller', function(orden){
-
+                orders.postDocuments(orden, orders.cbPostDocuments);
                 console.log('vela-controller :' + orden);
             });
 
             //Cuando recibo el evento pertiga-controller...
             socket.on('pertiga-controller', function(orden){
-
+                orders.postDocuments(orden, orders.cbPostDocuments);
                 console.log('Recibido evento pertiga-controller, con la data:' + orden);
-                atmospherics.postDocuments(orden, atmospherics.cbPostDocuments);
-
             });
 
 
@@ -48,10 +45,12 @@ module.exports = function Orders(){
             socket.on('sistems-controller', function(data){
                 if(sistemsStatus === true){
                     socket.emit('sistems-total-up', data);//TODO: Pasarle algo? En principio creo que dependiendo la fase o ensayo quizás haya que crear algo al respecto.
+                    orders.postDocuments(orden, orders.cbPostDocuments);
                     console.log(sistemsStatus);
                     sistemsStatus = false;
                 }else{
                     socket.emit('sistems-total-down', data+1)//TODO: Pasarle algo?
+                    orders.postDocuments(orden, orders.cbPostDocuments);
                     console.log(sistemsStatus);
                     sistemsStatus = true;
                 }
@@ -62,10 +61,12 @@ module.exports = function Orders(){
             socket.on('sistems-carena', function(data){
                 if(status === true){
                     socket.emit('sistems-carena-up', data);//TODO: Pasarle algo? En principio creo que dependiendo la fase o ensayo quizás haya que crear algo al respecto.
+                    orders.postDocuments(orden, orders.cbPostDocuments);
                     console.log(carenaStatus);
                     carenaStatus = false;
                 }else{
                     socket.emit('sistems-carena-down', data+1)//TODO: Pasarle algo?
+                    orders.postDocuments(orden, orders.cbPostDocuments);
                     console.log(carenaStatus);
                     carenaStatus = true;
                 }
@@ -76,10 +77,12 @@ module.exports = function Orders(){
             socket.on('sistems-heeled', function(data){
                 if(status === true){
                     socket.emit('sistems-heeled-up', data);//TODO: Pasarle algo? En principio creo que dependiendo la fase o ensayo quizás haya que crear algo al respecto.
+                    orders.postDocuments(orden, orders.cbPostDocuments);
                     console.log(heeledStatus);
                     heeledStatus = false;
                 }else{
                     socket.emit('sistems-heeled-own', data+1)//TODO: Pasarle algo?
+                    orders.postDocuments(orden, orders.cbPostDocuments);
                     console.log(heeledStatus);
                     heeledStatus = true;
                 }
@@ -90,10 +93,12 @@ module.exports = function Orders(){
             socket.on('sistems-dataCapture', function(data){
                 if(status === true){
                     socket.emit('sistems-dataCapture-up', data);//TODO: Pasarle algo? En principio creo que dependiendo la fase o ensayo quizás haya que crear algo al respecto.
+                    orders.postDocuments(orden, orders.cbPostDocuments);
                     console.log(dataCaptureStatus);
                     dataCaptureStatus = false;
                 }else{
                     socket.emit('sistems-dataCapture-own', data+1)//TODO: Pasarle algo?
+                    orders.postDocuments(orden, orders.cbPostDocuments);
                     console.log(dataCaptureStatus);
                     dataCaptureStatus = true;
                 }
